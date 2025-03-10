46b3c1a184c00bc31ed854391dc0691668c5362c
hieuhoang26 <hhh262434@gmail.com>
2025-03-10 15:36:25 +0700

    testt

---
+++ src/api/userDetail.js
@@ -0,0 +1,13 @@
+import http from "./http";
+
+const URL_UserGet = "user/user/";
+const URL_UserPut = "user/user/";
+
+export const userDetail = {
+  getUserById(id) {
+    return http.get(`${URL_UserGet}${id}`);
+  },
+  updateUser(id, userData) {
+    return http.put(`${URL_UserPut}${id}`, userData);
+  },
+};
